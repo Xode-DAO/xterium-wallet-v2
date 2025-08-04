c22387b89be22ed65efb856e4b7d6083029fc2a0
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-08-04 14:46:08 +0800

    template: welcome page

--- src/app/onboarding/welcome/welcome.page.ts
+++ src/app/onboarding/welcome/welcome.page.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCheckbox,
+  IonButton
+} from '@ionic/angular/standalone';
+
+import { HeaderComponent } from "../shared/header/header.component";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonCheckbox,
+    IonButton,
+    HeaderComponent
+]
 })
 export class WelcomePage implements OnInit {
 
