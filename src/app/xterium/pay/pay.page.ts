f35d629c41c1d9ffab637e7d8fd556fa1921d44c
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-08-06 18:05:09 +0800

    template: payment history

--- src/app/xterium/pay/pay.page.ts
+++ src/app/xterium/pay/pay.page.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-pay',
   templateUrl: './pay.page.html',
   styleUrls: ['./pay.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule]
 })
 export class PayPage implements OnInit {
 
--- src/app/xterium/transaction-history/transaction-history.page.ts
+++ src/app/xterium/transaction-history/transaction-history.page.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import {
+  IonContent,
+  IonSegment,
+  IonSegmentButton,
+  IonSegmentView,
+  IonSegmentContent,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonDatetime,
+  IonDatetimeButton,
+  IonCard,
+  IonAvatar,
+  IonModal
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-transaction-history',
   templateUrl: './transaction-history.page.html',
   styleUrls: ['./transaction-history.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonSegment,
+    IonSegmentButton,
+    IonSegmentView,
+    IonSegmentContent,
+    IonLabel,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonItem,
+    IonDatetime,
+    IonDatetimeButton,
+    IonCard,
+    IonAvatar,
+    IonModal
+  ]
 })
 export class TransactionHistoryPage implements OnInit {
 
   constructor() { }
+
+  selectedDate: string = new Date().toISOString();
 
   ngOnInit() {
   }
