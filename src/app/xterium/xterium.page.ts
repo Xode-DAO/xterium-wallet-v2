6b4697c112d901aac788747ef385b6cefe7f04d2
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-08-07 15:57:32 +0800

    template: import wallets for my-wallet modal

--- src/app/xterium/xterium.page.ts
+++ src/app/xterium/xterium.page.ts
@@ -14,13 +14,29 @@ import {
   IonIcon,
   IonModal,
   IonList,
-  IonItem,
   IonItemDivider,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonInput,
+  IonTextarea,
   IonLabel
 } from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
-import { addCircle, settingsOutline, close, briefcase, swapHorizontal, qrCode, timer, compass } from 'ionicons/icons';
+import {
+  addCircle,
+  settingsOutline,
+  close,
+  briefcase,
+  swapHorizontal,
+  qrCode,
+  timer,
+  compass,
+  copyOutline,
+  clipboardOutline
+} from 'ionicons/icons';
 
 import { WalletsComponent } from "./shared/wallets/wallets.component"
 
@@ -43,8 +59,13 @@ import { WalletsComponent } from "./shared/wallets/wallets.component"
     IonIcon,
     IonModal,
     IonList,
-    IonItem,
     IonItemDivider,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonItem,
+    IonInput,
+    IonTextarea,
     IonLabel,
     WalletsComponent
   ]
@@ -61,13 +82,29 @@ export class XteriumPage implements OnInit {
       qrCode,
       timer,
       compass,
+      copyOutline,
+      clipboardOutline
     });
   }
 
   presentingElement!: HTMLElement | null;
+  importPresentingElement!: HTMLElement | null;
+
+  mnemonicName: string = '';
+  mnemonicWords: string[] = new Array(12).fill('Sample');
+  privateKeyInput: string = '';
+
+  copyToClipboard() {
+
+  }
+
+  pasteFromClipboard() {
+
+  }
 
   ngOnInit() {
     this.presentingElement = document.querySelector('.xterium-content');
+    this.importPresentingElement = document.querySelector('.my-wallets');
   }
 
 }
