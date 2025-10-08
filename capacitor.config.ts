341b733c74c7ea3f3c13c077924f64f7f6aac610
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-10-08 11:21:22 +0800

    simulator: added the background runners

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.xterium.wallet.v2',
   appName: 'xterium-wallet-v2',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    BackgroundRunner: {
+      label: "com.smoldot.background.task",
+      src: "runners/smoldot.js",
+      event: "smoldotEvent",
+      repeat: true,
+      interval: 15,
+      autoStart: true,
+    },
+  },
 };
 
 export default config;
