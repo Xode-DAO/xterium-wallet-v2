8eb8aaf7187390b21b8cb71bd1fbcf22c1c9bb4e
Butch Canada <145413228+butchcanz@users.noreply.github.com>
2025-10-24 10:09:21 +0800

    feat: add content in swap page

--- src/app/xterium/swap/swap.page.ts
+++ src/app/xterium/swap/swap.page.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { IonContent, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { construct } from 'ionicons/icons';
+
 @Component({
   selector: 'app-swap',
   templateUrl: './swap.page.html',
   styleUrls: ['./swap.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonContent, IonIcon]
 })
 export class SwapPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      construct
+    });
+  }
 
   ngOnInit() {
   }
