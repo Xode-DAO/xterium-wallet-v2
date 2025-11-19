562400fd2d12a588cd0c7d3f40263fef36802278
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-11-19 14:27:06 +0800

    fix: status bar changes for android (for testing)

--- android/app/src/main/java/com/xterium/wallet/MainActivity.java
+++ android/app/src/main/java/com/xterium/wallet/MainActivity.java
@@ -2,4 +2,10 @@ package com.xterium.wallet;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setStatusBarColor(android.graphics.Color.parseColor("#1B1B1B"));
+  }
+}
